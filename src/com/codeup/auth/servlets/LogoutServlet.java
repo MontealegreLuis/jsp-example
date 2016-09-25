bddbca37dd0dda61386e44f07e9da34d7b8d6284
Luis Montealegre <montealegreluis@gmail.com>
2016-09-25 16:07:57 -0500

    Use configuration values for LogoutServlet.

--- src/com/codeup/auth/servlets/LogoutServlet.java
+++ src/com/codeup/auth/servlets/LogoutServlet.java
@@ -3,21 +3,28 @@
  */
 package com.codeup.auth.servlets;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LogoutServlet", urlPatterns = {"/logout"})
 public class LogoutServlet extends HttpServlet {
+    private String logoutPage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        logoutPage = config.getInitParameter("logoutPage");
+    }
+
     protected void doGet(
         HttpServletRequest request,
         HttpServletResponse response
     ) throws ServletException, IOException {
         request.getSession().removeAttribute("user");
         request.getSession().invalidate();
-        response.sendRedirect("/movies");
+        response.sendRedirect(logoutPage);
     }
 }
